33e39fe73e73899a207de98214558831c887601a
Montserrat Carrasco <mfcarrascot@gmail.com>
2024-04-18 03:03:38 -0400

    Función "buscar" añadida

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+const searchInput = document.querySelector(".search-city");
+const searchButton = document.querySelector(".search-button");
+searchButton.addEventListener("click", async () => {
+    const city = searchInput.value;
+    // TODO: usar api para encontrar data sobre el tiempo en una ciudad
+  });
+
+  
