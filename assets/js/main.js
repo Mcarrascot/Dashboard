637e7d2512493a4efd34b515774a4276eee186f2
Montserrat Carrasco <mfcarrascot@gmail.com>
2024-04-20 12:33:19 -0400

    Añadí UV Index e Información sobre locación actual

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,59 @@ import { getWeatherData } from "./api.js";
 
 const searchInput = document.querySelector(".search-city");
 const searchButton = document.querySelector(".search-button");
-searchButton.addEventListener("click", async () => {
+searchButton.addEventListener("click", async (event) => {
+    event.preventDefault(); // Evitar el comportamiento de envío de formulario predeterminado
     const ciudad = searchInput.value;
+    if (!ciudad) return; // No hay nada que buscar? retorna antes. 
+    // TODO: Mostrar "cargando" mientras se espera por información.
     const data = await getWeatherData(ciudad);
-    // TODO: Desplegar información tiempo en la página
-  });
+    // TODO: Dejar de mostrar "cargando".
+    printWeatherDataToScreen(data);
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const initialWeatherData = await getWeatherData('Concepcion');
+    console.log(initialWeatherData)
+    printWeatherDataToScreen(initialWeatherData);
+})
+
+
+const printWeatherDataToScreen = (weatherData) => {
+    // Mostrar datos meteorológicos actuales
+    const title = `Tiempo en ${weatherData.location.name}, ${weatherData.location.region}`;
+    document.getElementById("nombre-ciudad").textContent = title;
+    document.getElementById("temperatura").textContent = weatherData.current.temp_c;
+    document.getElementById("condición").textContent = weatherData.current.condition.text;
+    document.getElementById("velocidad-viento").textContent = weatherData.current.wind_kph;
+    document.getElementById("humedad").textContent = weatherData.current.humidity;
+
+    // UV index tabla
+    const uvIndexData = {
+        labels: ["UV Index"],
+        datasets: [{
+            label: "UV Index",
+            backgroundColor: "rgba(255, 206, 86, 0.2)",
+            borderColor: "rgba(255, 206, 86, 1)",
+            borderWidth: 1,
+            data: [weatherData.current.uv]
+        }]
+    };
+
+    const uvIndexCtx = document.getElementById("uvIndexChart").getContext("2d");
+    const uvIndexChart = new Chart(uvIndexCtx, {
+        type: "bar",
+        data: uvIndexData,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    max: 10
+                }
+            }
+        }
+    });
+}
+
 
   var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
